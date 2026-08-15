f1e1fde7d06544c46d52453bfcd0c2ecdd31be3c
Yehor <egorkudria32@gmail.com>
2026-08-15 16:57:22 +0200

    Remove test Azure Container App config

--- database/config.py
+++ database/config.py
@@ -1,9 +1,9 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise RuntimeError(
-        "DATABASE_URL environment variable is not set"
-    )
+    raise RuntimeError("DATABASE_URL environment variable is not set")
